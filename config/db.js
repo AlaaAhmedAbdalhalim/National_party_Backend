dec6d36f7f92d27888c74c19444e781e917aef47
Alaa Ahmed <alaaahmedabdelhalim@gmail.com>
2025-12-14 19:55:45 +0200

    Solving bugs

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.MYSQL_PUBLIC_URL) {
   throw new Error('DATABASE_URL is not defined');
 }
 
-const dbUrl = process.env.DATABASE_URL;
+const dbUrl = process.env.MYSQL_PUBLIC_URL;
 const myURL = new URL(dbUrl); // WHATWG URL
 
 const pool = mysql.createPool({
@@ -16,6 +16,6 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
-});
+});     
 
 module.exports = pool;
