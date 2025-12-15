bb654d791c731946f8581beeed4e674a1344684c
Alaa Ahmed <alaaahmedabdelhalim@gmail.com>
2025-12-15 19:27:19 +0200

    Make test

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
-app.get("/health", (req, res) => res.json({ status: "ok" }));
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.use("/api/events", require("./routes/eventsRoutes"));
 
 const PORT = process.env.PORT;
-app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log("Server running on port", PORT)
+);
