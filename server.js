19489ce5b2b4f1b7143f7e62c271ebb7001a2d9c
AlaaAhmedAbdalhalim <alaaahmedabdalhalim@gmail.com>
2025-12-18 13:05:01 +0200

    Adding Login & Auth

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const multer = require("multer");
-
+const authRoutes = require('./routes/authRoutes');
 const app = express();    //           لازم ييجي قبل أي app.get أو app.use
 
 // DB
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use('/api/events', eventsRouter);
 app.use('/api/news', newsRouter);
 app.use('/api/members', membersRouter); 
-
+app.use('/api/auth', authRoutes);
 // Start server
 const PORT = process.env.MYSQLPORT || 8080; 
 app.listen(PORT, "0.0.0.0", () => {
