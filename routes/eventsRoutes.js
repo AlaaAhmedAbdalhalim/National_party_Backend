3e5d3bd908529450c9365b5d41caa3955febd952
Alaa Ahmed <alaaahmedabdelhalim@gmail.com>
2025-12-23 19:06:52 +0200

    Add PUT & DELETE in Events Route

--- routes/eventsRoutes.js
+++ routes/eventsRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 const cloudinary = require("../config/cloudinary");
+const auth = require("../middleware/auth");
+const isAdmin = require("../middleware/isAdmin");
 
 // GET all events
 router.get("/", async (req, res) => {
@@ -41,6 +43,61 @@ router.post("/", async (req, res) => {
   } catch (error) {
     console.error("POST Event Error:", error); // هيطبع كل التفاصيل
     res.status(500).json({ message: "Failed to add event" });
+  }
+});
+
+router.put('/:id', auth, isAdmin, async (req, res) => {
+  const eventId = req.params.id;
+  const { Title, Description, Image , Date } = req.body;
+
+  try {
+    const [rows] = await db.query('SELECT * FROM Events WHERE id = ?', [eventId]);
+    if (rows.length === 0) {
+      return res.status(404).json({ message: 'Events not found' });
+    }
+
+    let imageUrl = rows[0].Image; // Default old image
+
+    // لو صورة جديدة مرفوعة Base64
+    if (Image && Image.startsWith('data:image')) {
+      // رفع على Cloudinary
+      const upload = await cloudinary.uploader.upload(Image, {
+        folder: 'events'
+      });
+      imageUrl = upload.secure_url;
+    }
+
+    // تحديث DB
+    await db.query(
+      'UPDATE Events SET Title = ?, Description = ?, Image = ?, Date = ? WHERE id = ?',
+      [Title, Description, imageUrl, Date, eventId]
+    );
+
+    res.status(200).json({ message: 'Events updated successfully' });
+  } catch (error) {
+    console.error('Update events error:', error);
+    res.status(500).json({ message: 'Server error', error: error.toString() });
+  }
+});
+
+
+
+router.delete('/:id', auth, isAdmin, async (req, res) => {
+  const events = req.params.id;
+
+  try {
+    const [rows] = await db.query('SELECT * FROM Events WHERE id = ?', [events]);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: 'Events not found' });
+    }
+
+    await db.query('DELETE FROM Events WHERE id = ?', [events]);
+
+    res.status(200).json({ message: 'Events deleted successfully' });
+  } catch (error) {
+    console.error('Delete events error:', error);
+    res.status(500).json({ message: 'Server error', error: error.toString() });
   }
 });
 
